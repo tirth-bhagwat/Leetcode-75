78937bb07a1f1d99f497d5f9e9e3b2486eb07b0c
tirth-bhagwat <tirth.bhagwat@proton.me>
2023-07-29 22:50:37 +0530

    add py script to auto create file from given leet-code url

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-pub mod common_divisor_of_strings;
-pub mod merge_strings_alternately;
-pub mod gcd;
+mod common_divisor_of_strings;
 mod find_greatest_common_divisor_of_array;
-pub mod kids_with_the_greatest_number_of_candies;
+mod find_numbers_with_even_number_of_digits;
+mod gcd;
+mod kids_with_the_greatest_number_of_candies;
+mod merge_strings_alternately;
 
 fn main() {
     println!("Hello, world!");
